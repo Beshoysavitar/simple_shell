efdc3034595820eaa06fe208adafc2139b817cf2
beshoy <besho.elking33377@gmail.com>
2023-08-23 11:19:08 -0700

    simple shell

--- main.c
+++ main.c
@@ -1,105 +1,90 @@
-#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
+void get_line(char *line, size_t size);
+void parse_line(char *line, char **args);
+int execute(char **args);
 /**
- * get_line - Get a line from the user.
- * @prompt: The prompt to display to the user.
- * @line: The buffer to store the line in.
- * @q: The size of the buffer.
+ * main - ***
  *
- * Return: the number of characters read, or -1 on error.
+ * Return: ***
  */
-static ssize_t get_line(const char *prompt, char *line, size_t q)
+int main()
 {
-	ssize_t nchars_read;
-
-	printf("%s", prompt);
-	nchars_read = getline(&line, &q, stdin);
-	if (nchars_read == -1)
-	{
-		perror("error");
-		return (-1);
-	}
-
-	return (nchars_read);
-}
-
-/**
- * parse_line - Parse a line into an array of arguments.
- * @line: The line to parse.
- * @d: The delimiter.
- *
- * Return: an array of arguments, or NULL on error.
- */
-static char **parse_line(char *line, const char *d)
-{
-	int x;
-	int num_t = 0;
-	char *t;
+	char *line;
 	char **args;
+	int status;
 
-	t = strtok(line, d);
-	while (t != NULL)
-	{
-		num_t++;
-		t = strtok(NULL, d);
-	}
-	num_t++;
-	args = malloc(sizeof(char *) * num_t);
-	if (args == NULL)
-	{
-		perror("error");
-		return (NULL);
-	}
-
-	t = strtok(line, d);
-	while (t != NULL)
-	{
-		args[x] = malloc(sizeof(char) * strlen(t));
-		strcpy(args[x], t);
-		x++;
-		t = strtok(NULL, d);
-	}
-
-	args[num_t - 1] = NULL;
-
-	return (args);
-}
-
-/**
- * main - The main function of the shell.
- * @ac: The number of arguments.
- * @argv: The arguments.
- *
- * Return: 0 on success, or -1 on error.
- */
-int main(int ac, char **argv)
-{
-	char *lin = NULL;
-	char **args;
-	ssize_t nchars_read;
-
-	(void)ac;
-	(void)argv;
 	while (1)
 	{
-		nchars_read = get_line("#cisfun$ ", lin, 1024);
-		if (nchars_read == -1)
+		get_line(line, 1024);
+		if (line == NULL)
 		{
-			return (-1);
+			break;
 		}
 
-		args = parse_line(lin, " \n");
-		if (args == NULL)
-		{
-			perror("error");
-			return (-1);
-		}
+		parse_line(line, args);
+		status = execute(args);
 
-		exe(args);
-
+		free(line);
 		free(args);
-		free(lin);
 	}
 
-	return (0);
+	return 0;
+}
+/**
+ * get_line - ***
+ * @line: ***
+ * @size: ***
+ *
+ * Return: ***
+ */
+void get_line(char *line, size_t size)
+{
+	printf("$ ");
+	getline(line, &size, stdin);
+}
+/**
+ * parse_line - ***
+ * @line: ***
+ * @args: ***
+ *
+ * Return: ***
+ */
+void parse_line(char *line, char **args)
+{
+	int i = 0;
+	for (char *token = strtok(line, " "); token != NULL; token = strtok(NULL, " "))
+	{
+		args[i] = token;
+		i++;
+	}
+	args[i] = NULL;
+}
+/**
+ * main - ***
+ * @args: ***
+ *
+ * Return: ***
+ */
+int execute(char **args)
+{
+	if (strcmp(args[0], "exit") == 0)
+	{
+		return 0;
+	}
+	else if (strcmp(args[0], "env") == 0)
+	{
+		for (char **env = environ; *env != NULL; env++)
+		{
+			printf("%s\n", *env);
+		}
+		return 1;
+	}
+	else
+	{
+		return execve(args[0], args, environ);
+	}
 }
